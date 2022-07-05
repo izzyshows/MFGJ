f6498ce738fa0cbc7c8b9f5910491f6ab4eb7cf5
Glusoft <vulcain_nico@hotmail.fr>
2022-07-05 02:44:12 +0200

    Add movement + attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 currentInputVector;
-    private Vector2 smoothInputVelocity;
-
-    [SerializeField]
-    private float smoothInputSpeed = .2f;
-
     [SerializeField]
     private float playerSpeed = 10f;
-
-    private PlayerInput playerInput;
-    private InputAction moveAction;
-    private InputAction fireAction;
 
     [SerializeField]
     private GameObject sword;
@@ -26,15 +15,12 @@ public class PlayerController : MonoBehaviour
 
 
     void Start() {
-        playerInput = GetComponent<PlayerInput>();
-        moveAction = playerInput.actions["Move"];
-        fireAction = playerInput.actions["Fire"];
     }
 
     void Update() {
 
         // attack
-        if(fireAction.triggered && !attackEnabled) {
+        if(Input.GetButton("Fire1") && !attackEnabled) {
             attackEnabled = true;
             bufferAttack = 0;
             sword.SetActive(true);
@@ -49,9 +35,8 @@ public class PlayerController : MonoBehaviour
         }
 
         // movement
-        Vector2 inputMove = moveAction.ReadValue<Vector2>();
-        currentInputVector = Vector2.SmoothDamp(currentInputVector, inputMove, ref smoothInputVelocity, smoothInputSpeed);
-        transform.position = new Vector3(transform.position.x + currentInputVector.x * playerSpeed * Time.deltaTime, transform.position.y + currentInputVector.y * playerSpeed * Time.deltaTime);
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position = new Vector3(transform.position.x + input.x * playerSpeed * Time.deltaTime, transform.position.y + input.y * playerSpeed * Time.deltaTime);
     }
     
     private Inventory inventory;
