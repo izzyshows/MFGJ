9c4abee1ad939c7497965e7ea0c771a758cc1211
izzyshows <izzyshows@hotmail.com>
2022-07-13 10:26:51 -0400

    save

--- Assets/Scripts/ShowHideInventory.cs
+++ Assets/Scripts/ShowHideInventory.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class ShowHideInventory : MonoBehaviour
 {
     public GameObject theInventory;
+    //private bool isActive;
 
     void Update()
     {
         if (Input.GetKeyDown("i") && theInventory.activeInHierarchy)
         {
-            gameObject.GetComponent<CanvasRenderer>().cull = false;
+            theInventory.SetActive(false);
         }
         if (Input.GetKeyDown("i") && !theInventory.activeInHierarchy)
         {
             theInventory.SetActive(true);
         }
     }
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.I))
+    //    {
+    //        if (isActive)
+    //        {
+    //            isActive = false;
+    //        }
+    //        else
+    //        {
+    //            isActive = true;
+    //        }
+
+    //        theInventory.SetActive(isActive);
+    //    }
+
+    //}
 }
